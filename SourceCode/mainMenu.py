c1c3a7b334c63f4835ac11d95238846846cc6de2
Nguyễn Phúc Thuần <phucthuan1st@gmail.com>
2020-12-30 20:19:57 +0700

    add login menu and new music

--- SourceCode/mainMenu.py
+++ SourceCode/mainMenu.py
@@ -6,7 +6,8 @@ from pygame.locals import *
 
 from mainGame import *
 
-import openpyxl
+import pandas as pd
+
 '''anything go with rect use the form (left, top, width, height)'''
 pygame.init()
 
@@ -15,23 +16,31 @@ FPS = 60
 fpsClock = pygame.time.Clock()
 gMoney = 0
 
+#access to database
+database = pd.ExcelFile("../database.xlsx")
+data = pd.read_excel(database, 0, header=None)
+
+#windows statics
 WINDOWSIZE = (1280,720) #window size
 pygame.display.set_caption('Racing bet 888') #set Caption for title bar
-menuSound = pygame.mixer.Sound('..\soundFX\menu.wav') #open sound
+menuSound = pygame.mixer.Sound('../soundFX/menu.wav') #open sound
 DISPLAYSURFACE = pygame.display.set_mode(WINDOWSIZE) #create surface for mainmenu
-loginscreen = pygame.image.load('..\image\loginscreen.png')
+loginscreen = pygame.image.load('../image/loginscreen.png')
+changeSet = pygame.image.load('../image/changeSet.png')
+donate = pygame.image.load('../image/donateRaiseRacingGame.png')
 
 #define the set image
-set0 = '..\image\set0.png'
-set1 = '..\image\set1.png'
-set2 = '..\image\set2.png'
-set3 = '..\image\set3.png'
-set4 = '..\image\set4.png'
-set5 = '..\image\set5.png'
+set0 = '../image/set0.png'
+set1 = '../image/set1.png'
+set2 = '../image/set2.png'
+set3 = '../image/set3.png'
+set4 = '../image/set4.png'
+set5 = '../image/set5.png'
 setIndex = [set0, set1, set2, set3, set4, set5]
 characterSet = 0
-loginSound = pygame.mixer.Sound('..\soundFX\menu.wav')
-#define font using 
+loginSound = pygame.mixer.Sound("../soundFX/loginsound.wav")
+loginScreen = pygame.image.load("../image/loginscreen.png")
+
 font = pygame.font.SysFont(None, 20, bold=True, italic=False) #set font for drawing
 userNameFont = pygame.font.SysFont(None, 25, bold=True, italic=True)
 mediumfont = pygame.font.SysFont(None, 30, bold = True, italic = False)
@@ -41,26 +50,61 @@ bigfont = pygame.font.SysFont(None, 40, bold = True, italic = False)
 #------------------------------------------------------------------------------------------------#
 
 #drawing text on screen
-def draw_text(text, font, color, surface, x, y): 
+def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x,y)
     surface.blit(textobj, textrect)
     return 1
-loginScreen = pygame.image.load('..\image\loginscreen.png')
+
 
 def loginscreen():
     running = True
     clicked = False
+    loginSound.play(-1)
+    show = True
+    inputUserName = ""
+    inputPassword = ""
+    censoredPassword = ""
+    typingUserName = False
+    typingPassword = False
+    pushLoginButtn = False
     while running:
         DISPLAYSURFACE = pygame.display.set_mode(WINDOWSIZE)
-        DISPLAYSURFACE.blit(loginScreen, (0,0))
-
-        userNameArea = pygame.Rect(40, 320, 375, 40)
-
+        DISPLAYSURFACE.blit(loginScreen, (0, 0))
+        DISPLAYSURFACE.blit(donate, (1080, 0))
+        draw_text('DONATE TO HELP THE DEVELOPMENT', font, (255,255,255), DISPLAYSURFACE, 1000, 200)
+        userNameArea = pygame.Rect(40, 320, 375, 37)
+        passwordArea = pygame.Rect(40, 397, 374, 40)
+        loginButton = pygame.Rect(312, 460, 99, 32)
         dx, dy = pygame.mouse.get_pos()
 
+        if show:
+            draw_text('Now Playing: NIVIRO - Demons (No Copyright Sound)', font, (255,255,255), DISPLAYSURFACE, 1, 705)
+        show = not show
+
+        if userNameArea.collidepoint(dx, dy):
+            pygame.draw.rect(DISPLAYSURFACE, (0, 255, 0), userNameArea, 3)
+            if clicked:
+                typingUserName = True
+                typingPassword = False
+                print(inputUserName)
+        if passwordArea.collidepoint(dx, dy):
+            pygame.draw.rect(DISPLAYSURFACE, (0, 255, 0), passwordArea, 3)
+            if clicked:
+                typingPassword = True
+                typingUserName = False
+                print(inputPassword)
+        if loginButton.collidepoint(dx, dy):
+            pygame.draw.rect(DISPLAYSURFACE, (0, 255, 0), loginButton, 3)
+            if clicked:
+                TypingPassword = False
+                TypingUserName = False
+                pushLoginButtn = True
+
+
         clicked = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -70,20 +114,49 @@ def loginscreen():
                     clicked = True
             if event.type == KEYDOWN:
                 if event.key == K_RETURN:
+                    if inputUserName == "" or inputPassword == "":
+                        pushLoginButtn = False
+                    else:
+                        running = False
+                        username = inputUserName
+                        password = inputPassword
+                else:
+                    if typingUserName and not typingPassword:
+                        if event.key == K_BACKSPACE:
+                            inputUserName = inputUserName[0:-1]
+                        else:
+                            inputUserName += event.unicode
+                    elif typingPassword and not typingUserName:
+                        if event.key == K_BACKSPACE:
+                            inputPassword = inputPassword[0:-1]
+                            censoredPassword = censoredPassword[0:-1]
+                        else:
+                            inputPassword += event.unicode
+                            censoredPassword += '*'
+            if pushLoginButtn:
+                if inputUserName == "" or inputPassword == "":
+                    pushLoginButtn = False
+                else:
                     running = False
+                    username = inputUserName
+                    password = inputPassword
 
+            draw_text(inputUserName, font, (0,0,0), DISPLAYSURFACE, 45, 330)
+            draw_text(censoredPassword, font, (0,0,0), DISPLAYSURFACE, 45, 407)
         fpsClock.tick(FPS)
         pygame.display.update()
-    username = 'Thuan dep trai'
-    return username
+    loginSound.stop()
+    return username, password
+
 
 #running main menu
 def mainMenu(money, characterSet, username):
-    Running = True
-    clicked = False
-    toggleMenuSub = False
-    menuSound.play(-1)
+    Running = True #check if running
+    clicked = False #get clicked
+    show = True #music description info
+    menuSound.play(-1) #playing background music
     while Running:
+        #define the display
         MAINMENUSCREEN = pygame.image.load(setIndex[characterSet])
         MAINMENUSCREEN = pygame.transform.scale(MAINMENUSCREEN, WINDOWSIZE)
         DISPLAYSURFACE.blit(MAINMENUSCREEN, (0,0)) #draw background
@@ -94,16 +167,28 @@ def mainMenu(money, characterSet, username):
         pygame.draw.rect(DISPLAYSURFACE, (255, 0, 0), moneyArea, 3)
         draw_text(username, userNameFont, (255, 0, 255), DISPLAYSURFACE, 260, 100)
         draw_text(str(money), mediumfont, (255,0,0), DISPLAYSURFACE, 700, 630)
-        draw_text('YOUR CURRENT SET IS: ' + str(characterSet), font, (0,0,0), DISPLAYSURFACE, 550, 200)
 
-        #define the Buttons
-        exitButton = pygame.Rect(40, 38, 82, 67)
+        #define the bet button
+        bet1Button = pygame.Rect(5, 298, 200, 153)
+        bet2Button = pygame.Rect(210, 298, 200, 153)
+        bet3Button = pygame.Rect(410, 298, 200, 153)
+        bet4Button = pygame.Rect(610, 298, 200, 153)
+        bet5Button = pygame.Rect(810, 298, 280, 153)
+        bet6Button = pygame.Rect(1010, 298, 280, 153)
+
+        #show the music description
+        if show:
+            draw_text('Now Playing: Linko - Goodbye (No Copyright Sound)', font, (255,255,0), DISPLAYSURFACE, 500, 705)
+        show = not show
+
+        #define the Buttons used in main menu
+        exitButton = pygame.Rect(58, 42, 82, 67)
         helpButton = pygame.Rect(55, 580, 110, 100)
         miniGameButton = pygame.Rect(212, 575, 100, 100)
-        changeSetButton = pygame.Rect(350, 580, 110, 100)
-        shopButton = pygame.Rect(885, 580, 90, 95)
+        changeSetButton = pygame.Rect(360, 580, 110, 100)
+        shopButton = pygame.Rect(888, 582, 93, 95)
         gameButton = pygame.Rect(1050, 580, 210, 100)
-        playButton = pygame.Rect(1075, 470, 120, 40)
+        playButton = pygame.Rect(1075, 465, 120, 45)
         changeNameButton = pygame.Rect(1075, 515, 120, 40)
 
         #GET MOUSE CLICK
@@ -115,7 +200,6 @@ def mainMenu(money, characterSet, username):
         else:
             frame = (255,255,255)
         if exitButton.collidepoint(dx, dy):
-            pygame.draw.rect(DISPLAYSURFACE, frame, exitButton, 3)
             if clicked:
                 exitConfirmScreen()
         if helpButton.collidepoint(dx, dy):
@@ -137,17 +221,13 @@ def mainMenu(money, characterSet, username):
         if gameButton.collidepoint(dx, dy):
             pygame.draw.rect(DISPLAYSURFACE, frame, gameButton, 3)
             if clicked:
-                toggleMenuSub = not toggleMenuSub
-        if playButton.collidepoint(dx, dy):
-            if toggleMenuSub:
-                if clicked:
-                    if characterSet == 0:
-                        characterSet = 1
-                    money = runGame(2, characterSet, money)
-        if changeNameButton.collidepoint(dx, dy):
-            if toggleMenuSub:
-                if clicked:
-                    draw_text('PRESSED', mediumfont, frame, DISPLAYSURFACE, 500, 500)
+                if characterSet == 0:
+                    characterSet = 1
+                money = runGame(2, characterSet, money)
+                menuSound.play(-1)
+
+
+        #not code yet
         clicked = False
 
     #checking exit game or input mouse click
@@ -158,14 +238,12 @@ def mainMenu(money, characterSet, username):
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     clicked = True
-    #if menusub is on then draw it
-        if toggleMenuSub:
-            drawGameMenuSub()
 
     #update screen every frame of loop
         fpsClock.tick(FPS)
         pygame.display.update() #update screen every execution
     return Running #return the running status to main
+
 
 def exitConfirmScreen():
     running = True
@@ -206,11 +284,13 @@ def exitConfirmScreen():
         pygame.display.update()
     return running
 
+
 def drawHelp():
     draw_text('HELP', bigfont, (255,255,255), DISPLAYSURFACE, 620, 20)
     draw_text('Welcome to Racing Bet Game', mediumfont, (255,255,255), DISPLAYSURFACE, 500, 50)
     draw_text('Nothing to see here at this time', font, (255,255,255), DISPLAYSURFACE, 550, 100)
     draw_text('Press ESC Key to return Main Menu', font, (255,255,255), DISPLAYSURFACE, 530, 120)
+
 
 def helpScreen():
     running = True
@@ -227,6 +307,7 @@ def helpScreen():
                     running = False
         fpsClock.tick(FPS)
         pygame.display.update()
+
 
 def miniGameScreen(money):
     running = True
@@ -247,18 +328,54 @@ def miniGameScreen(money):
         pygame.display.update()
     return money
 
+
 def miniGameEvent(money):
     money += 10
     return money
 
+
 def changeSetScreen(selectedSet):
     running = True
+    clicked = False
     while running:
-        DISPLAYSURFACE.fill((0,0,0))
-        draw_text('CHOSE YOUR FAVORITE SET: ', bigfont, (255,255,255), DISPLAYSURFACE, 400, 50)
-        draw_text('YOUR CURRENT SET IS: ' + str(selectedSet), mediumfont, (255,255,255), DISPLAYSURFACE, 450, 100)
-        draw_text('Press 1 to 5 to choose set', mediumfont, (255,255,255), DISPLAYSURFACE, 480, 150)
-        draw_text('Press ESC Key to return Main Menu', mediumfont, (255,255,255), DISPLAYSURFACE, 415, 200)
+        DISPLAYSURFACE.blit(changeSet, (0,0))
+
+        draw_text('Your current set is: ' + str(selectedSet), font, (0,0,0), DISPLAYSURFACE, 500, 200)
+
+        if clicked:
+            running = False
+
+        set1Button = pygame.Rect(5, 298, 200, 153)
+        set2Button = pygame.Rect(230, 298, 200, 153)
+        set3Button = pygame.Rect(465, 298, 200, 153)
+        set4Button = pygame.Rect(710, 298, 200, 153)
+        set5Button = pygame.Rect(950, 298, 280, 153)
+
+        dx, dy = pygame.mouse.get_pos()
+
+        if set1Button.collidepoint(dx, dy):
+            pygame.draw.rect(DISPLAYSURFACE, (0,0,0), set1Button, 3)
+            if clicked:
+                selectedSet = 1
+        if set2Button.collidepoint(dx, dy):
+            pygame.draw.rect(DISPLAYSURFACE, (0,0,0), set2Button, 3)
+            if clicked:
+                selectedSet = 2
+        if set3Button.collidepoint(dx, dy):
+            pygame.draw.rect(DISPLAYSURFACE, (0,0,0), set3Button, 3)
+            if clicked:
+                selectedSet = 3
+        if set4Button.collidepoint(dx, dy):
+            pygame.draw.rect(DISPLAYSURFACE, (0,0,0), set4Button, 3)
+            if clicked:
+                selectedSet = 4
+        if set5Button.collidepoint(dx, dy):
+            pygame.draw.rect(DISPLAYSURFACE, (0,0,0), set5Button, 3)
+            if clicked:
+                selectedSet = 5
+
+        clicked = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -276,19 +393,24 @@ def changeSetScreen(selectedSet):
                     selectedSet = 5
                 if event.key == K_ESCAPE:
                     running = False
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    clicked = True
 
         fpsClock.tick(FPS)
         pygame.display.update()
     return selectedSet
 
+dontHavemoney = 'YOU DON\'T HAVE ENOUGH MONEY'
+
 def shopScreen(money):
     running = True
     while running:
-        DISPLAYSURFACE.fill((0,0,0))
-        draw_text('Nothing at this time', bigfont, (255,255,255), DISPLAYSURFACE, 470, 300)
-        draw_text('Money at this time is: ' + str(money), mediumfont, (255,255,255), DISPLAYSURFACE, 490, 350)
+        DISPLAYSURFACE.fill((0, 0, 0))
+        draw_text('Nothing at this time', bigfont, (255, 255, 255), DISPLAYSURFACE, 470, 300)
+        draw_text('Money at this time is: ' + str(money), mediumfont, (255, 255, 255), DISPLAYSURFACE, 490, 350)
         draw_text('Press ESC Key to return Main Menu', font, (255,255,255), DISPLAYSURFACE, 490, 200)
-        draw_text('Press 1 to 5 to buy', font, (255,255,255), DISPLAYSURFACE, 550, 400)
+        draw_text('Press 1 to 5 to buy', font, (255, 255, 255), DISPLAYSURFACE, 550, 400)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -296,51 +418,61 @@ def shopScreen(money):
             if event.type == KEYDOWN:
                 if event.key == ord('1'):
                     if money < 100:
-                        draw_text('YOU DON\'T HAVE ENOUGH MONEY', bigfont, (255,255,255), DISPLAYSURFACE, 400, 500)
+                        draw_text(dontHavemoney, bigfont, (255, 255, 255), DISPLAYSURFACE, 400, 500)
                     else:
                         money -= 100
                 if event.key == ord('2'):
                     if money < 200:
-                        draw_text('YOU DON\'T HAVE ENOUGH MONEY', bigfont, (255,255,255), DISPLAYSURFACE, 400, 500)
+                        draw_text(dontHavemoney, bigfont, (255, 255, 255), DISPLAYSURFACE, 400, 500)
                     else:
                         money -= 200
                 if event.key == ord('3'):
                     if money < 300:
-                        draw_text('YOU DON\'T HAVE ENOUGH MONEY', bigfont, (255,255,255), DISPLAYSURFACE, 400, 500)
+                        draw_text(dontHavemoney, bigfont, (255, 255, 255), DISPLAYSURFACE, 400, 500)
                     else:
                         money -= 300
                 if event.key == ord('4'):
                     if money < 400:
-                        draw_text('YOU DON\'T HAVE ENOUGH MONEY', bigfont, (255,255,255), DISPLAYSURFACE, 400, 500)
-                    else: 
+                        draw_text(dontHavemoney, bigfont, (255, 255, 255), DISPLAYSURFACE, 400, 500)
+                    else:
                         money -= 400
                 if event.key == ord('5'):
                     if money < 500:
-                        draw_text('YOU DON\'T HAVE ENOUGHT MONEY', bigfont, (255,255,255), DISPLAYSURFACE, 400, 500)
-                    else: 
-                        money -= 500                       
+                        draw_text(dontHavemoney, bigfont, (255, 255, 255), DISPLAYSURFACE, 400, 500)
+                    else:
+                        money -= 500
                 if event.key == K_ESCAPE:
                     running = False
         fpsClock.tick(FPS)
         pygame.display.update()
     return money
 
-def drawGameMenuSub():
+
+def drawGameMenuSub(set):
+    if set == 1:
+        textColor = (0,0,0)
+        buttonColor = (255,255,255)
+    else:
+        textColor = (255, 255, 255)
+        buttonColor = (0, 0, 0)
     playButton = pygame.Rect(1075, 470, 120, 40)
     changeNameButton = pygame.Rect(1075, 515, 120, 40)
-    pygame.draw.rect(DISPLAYSURFACE, (0,0,0), playButton, 3)
-    pygame.draw.rect(DISPLAYSURFACE, (0,0,0), changeNameButton, 3)
-    draw_text('PLAY', font, (0,0,0), DISPLAYSURFACE, 1115, 485)
-    draw_text('CHANGE NAME', font, (0,0,0), DISPLAYSURFACE, 1080, 530)
+    pygame.draw.rect(DISPLAYSURFACE, buttonColor, playButton)
+    pygame.draw.rect(DISPLAYSURFACE, buttonColor, changeNameButton)
+    draw_text('PLAY', font, textColor, DISPLAYSURFACE, 1115, 485)
+    draw_text('CHANGE NAME', font, textColor, DISPLAYSURFACE, 1080, 530)
+
 
 def changeNameScreen():
     pass
 
+
 def main():
-    username = loginscreen()
+    username, password = loginscreen()
     mainMenu(gMoney, characterSet, username)
+
 
 if __name__ == "__main__":
     main()
 
-#end of file
+# end of file
